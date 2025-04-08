54c609a9c670ff0cc87672d803a5ea7b432f322c
FrancescoCesari97 <148258227+FrancescoCesari97@users.noreply.github.com>
2025-04-08 11:27:02 +0200

    changed step back animation

--- Assets/Scripts/Character/Player/PlayerLocomotionManager.cs
+++ Assets/Scripts/Character/Player/PlayerLocomotionManager.cs
@@ -160,7 +160,7 @@ public class PlayerLocomotionManager : CharacterLocomotionManager
         else 
         {
             // perform a backstep animation
-            player.playerAnimatorManager.PlayTargetActionAnimation("Dodging Back", true, true);
+            player.playerAnimatorManager.PlayTargetActionAnimation("Standing Dodge Backward", true, true);
         }
     }
 }
